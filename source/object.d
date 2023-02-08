82a8d93f378cb17d7b13d31a2b414d609894312f
Brendon Shaw <brendon.shaw8921@gmail.com>
2023-02-08 14:45:36 -0500

    work on primitives, add MariPrimitiveType and MariPrimitiveValue

--- source/object.d
+++ source/object.d
@@ -1,22 +1,57 @@
 import std.string;
 import std.conv;
 
+union MariPrimitiveValue {
+    int intValue;
+    float flValue;
+    string strValue;
+}
+
 enum MariPrimitiveType {
-    
+    NONE = 0,
+    INT,
+    FLOAT,
+    STRING
 }
 
 class MariObject {
 
-    private bool primitive;
-    private int primitiveIntValue;
-    private float primitiveFlValue;
-    private string primitiveStrValue;
+    private bool isPrimitive;
+    private MariPrimitiveType primitiveType;
+    private MariPrimitiveValue primitiveValue;
 
-    public bool isPrimitive(){
-        return primitive;
+    //integer primitive constructor
+    this(int value) {
+        MariPrimitiveValue pv;
+        this.primitiveType = MariPrimitiveType.INT;
+        pv.intValue = value;
+        this.primitiveValue = pv;
+        this.isPrimitive = true;
     }
-    public void setPrimitive(bool v) {
-        this.primitive = v;
+
+    //float primitive constructor
+    this(float value){
+        MariPrimitiveValue pv;
+        this.primitiveType = MariPrimitiveType.FLOAT;
+        pv.floatValue = value;
+        this.primitiveValue = pv;
+        this.isPrimitive = true;
+    }
+
+    //string primitive value
+    this(string value){
+        MariPrimitiveValue pv;
+        this.primitiveType = MariPrimitiveType.STRING;
+        pv.stringValue = value;
+        this.primitiveValue = pv;
+        this.isPrimitive = true;
+    }
+
+    public bool getIsPrimitive(){
+        return this.isPrimitive;
+    }
+    public void setIsPrimitive(bool v) {
+        this.isPrimitive = v;
     }
 
     //Methods that are specifically for non-primitive objects
