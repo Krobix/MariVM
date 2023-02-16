14d7e6dd7e7f633fd5ba44353422268ecf9f73e3
Brendon Shaw <brendon.shaw8921@gmail.com>
2023-02-16 09:24:51 -0500

    move Context to its own file

--- source/vm.d
+++ source/vm.d
@@ -5,93 +5,9 @@ import std.stdio;
 import std.array;
 import instruction;
 import mariobject;
+import context;
 
 //VM implementation
-
-class Context {
-    private int ip; //instruction pointer
-    private string ctxIdentifier;
-    private MariObject[10] registers;
-    private MariObject[] stack;
-    private int[string] varTable;
-    private Context parentContext;
-
-    this(string id){
-        this.ctxIdentifier = id;
-        this.stack = new MariObject[1];
-        this.parentContext = null;
-        this.ip = 0;
-    }
-
-    this(string id, Context ctx){
-        //this constructor is used to create a child Context
-        this.ctxIdentifier = id;
-        this.stack = new MariObject[1];
-        this.parentContext = ctx;
-        this.ip = 0;
-    }
-
-    public void setReg(int index, MariObject value){
-        this.registers[index] = value;
-    }
-
-    public MariObject getReg(int index){
-        return this.registers[index];
-    }
-
-    public MariObject getVarValue(string name){
-        return this.stack[this.varTable[name]];
-    }
-
-    public int getVarAddress(string name){
-        return this.varTable[name];
-    }
-
-    public void setVar(string name, int address){
-        this.varTable[name] = address;
-    }
-
-    public void pushToStack(MariObject obj){
-        this.stack ~= obj;
-    }
-
-    public MariObject getFromStack(int index){
-        return this.stack[index];
-    }
-
-    public MariObject getFromTopOfStack(){
-        return this.stack[$-1];
-    }
-
-    public void deleteFromStack(int index){
-        this.stack = remove(this.stack, index);
-    }
-
-    public void deleteFromTopOfStack(){
-        this.deleteFromStack(to!int(this.stack.length-1));
-    }
-
-    public Context getParent(){
-        return this.parentContext;
-    }
-
-    public string getIdentifier(){
-        return this.ctxIdentifier;
-    }
-
-    public int getIP(){
-        return this.ip;
-    }
-
-    public int incrementIP(){
-        return ++this.ip;
-    }
-
-    public int setIP(int i){
-        this.ip = i;
-        return this.ip;
-    }
-}
 
 class VM {
 
@@ -139,7 +55,7 @@ class VM {
                 return ctx.getReg(param.value.registerAddress);
             }
             else if(param.type==InstructionParamType.VARNAME){
-                foreach(string name; ctx.varTable.byKey()) {
+                foreach(string name; ctx.getVarTable().byKey()) {
                     if(name==param.value.varName){
                         return ctx.getVarValue(param.value.varName);
                     }
